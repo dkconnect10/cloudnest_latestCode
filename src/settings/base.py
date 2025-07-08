13a1afef1507395bb5845f9845961d44ba6fab26
Dilip Nowawave <dilip.nowawave@gmail.com>
2025-07-08 17:52:03 +0530

    Update STATIC_ROOT and MEDIA_ROOT paths to simplify directory structure

--- src/settings/base.py
+++ src/settings/base.py
@@ -186,7 +186,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'src/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -201,7 +201,7 @@ AUTH_USER_MODEL = 'users.User'
 # media setup
 import os
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'src/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # swwager parmission 
